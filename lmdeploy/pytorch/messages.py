b8564bf274242d6f3163f1803c7cdb96eb6838f7
RunningLeon <mnsheng@yeah.net>
2025-03-27 23:31:48 +0800

    fix update_tokens with multimodals

--- lmdeploy/pytorch/messages.py
+++ lmdeploy/pytorch/messages.py
@@ -631,7 +631,8 @@ class SchedulerSequence:
 
         # update multimodals
         if multimodals is not None:
-            multimodals = HistoryMultiModals.update_multimodals(multimodals, self.num_all_ids)
+            print(multimodals)
+            multimodals = HistoryMultiModals.update_multimodals(multimodals, self._num_history_ids)
             self.history_multimodals.add_inputs(multimodals)
 
         # cross
@@ -675,9 +676,11 @@ class SchedulerSequence:
             self._num_cross = self.history_multimodals.get_encoder_len(self._num_history_ids, num_all_ids)
 
     def __repr__(self):
-        repr = f'SchedulerSequence(seq_id={self.seq_id}, status={self.status}, history_len={self.history_len}, '
+        repr = f'SchedulerSequence(seq_id={self.seq_id}, histokens_num={len(self.history_cache)}, status={self.status}, history_len={self.history_len}, _num_token_ids={self._num_token_ids}, num_new_tokens={self.num_new_tokens},'
+        repr += f'all_tokens={self.history_cache[:self.num_all_ids]}, '
         repr += f'num_all_ids={self.num_all_ids}, num_blocks={self.num_blocks}, blocks={self.logical_blocks.get_real_blocks()}, '
         repr += f'multimodals_ranges={self.history_multimodals._mm_ranges}, last_shared_node={getattr(self.logical_blocks, "last_shared_node", None)}'
         return repr
+    
     def __str__(self):
         return self.__repr__()
