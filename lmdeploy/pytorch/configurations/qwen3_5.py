72b1d8be0c9f3448fda21365edfb4cf84f6e45fd
RangiLyu <lyuchqi@gmail.com>
2026-04-28 14:59:38 +0800

    freeze interns2 mtp delivery code

--- lmdeploy/pytorch/configurations/qwen3_5.py
+++ lmdeploy/pytorch/configurations/qwen3_5.py
@@ -1,7 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 
-from lmdeploy.pytorch import envs as _envs
 from lmdeploy.utils import is_bf16_supported
 
 from .builder import AutoModelConfigBuilder
@@ -64,8 +63,7 @@ class Qwen3_5ModelConfigBuilder(AutoModelConfigBuilder):
             dtype = torch.bfloat16
         else:
             dtype = torch.float16
-        ssm_dtype = dtype if not _envs.fp32_mamba_ssm_dtype else torch.float32
-        cfg.states_shapes = [(conv_state_shape, dtype), (recurrent_state_shape, ssm_dtype)]
+        cfg.states_shapes = [(conv_state_shape, dtype), (recurrent_state_shape, torch.float32)]
         cfg.is_gated_delta = True
         cfg.check_env_func = _check_env_qwen3_next
 
